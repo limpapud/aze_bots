8ed28c428dba8ac251d4d6cd8b884be339c6c605
Omar Bayramov <30339109+limpapud@users.noreply.github.com>
2019-01-14 01:11:14 +0400

    Few grammar changes in files.

--- adminBOT/config.py
+++ adminBOT/config.py
@@ -1,13 +1,13 @@
 # API configuration
-token = 'TOKEN' #add token here
+token = 'TOKEN'  # add token here
 
-#BAT file folder
+# BAT file folder
 script_fd = '/BATs/'
 
-#Backup folder configuration
-vbackup_logfolder='/BackUps/videolog/'
-mysqlbackup_logfolder='/BackUps/mysqlbackup/'
+# Backup folder configuration
+vbackup_logfolder = '/BackUps/videolog/'
+mysqlbackup_logfolder = '/BackUps/mysqlbackup/'
 
 # Database configuration
-adminBOTDB='/adminBOT.db'
-##md5salt = 'limpapud'
+adminBOTDB = '/adminBOT.db'
+# md5salt = 'limpapud'
